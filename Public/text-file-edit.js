881f0877528bb938e6b983f1ec56199a70b00575
unknown <teridax2@gmail.com>
2017-11-30 16:56:05 -0800

    Made progress on text-file-edit.js

--- Public/text-file-edit.js
+++ Public/text-file-edit.js
@@ -1,5 +1,10 @@
 var clientText = document.getElementById('text-content').innerHTML;
 
+//Inserts substring into string at index
+function strInsert(string, index, substring){
+  return string.substring(0,index) + substring + string.substring(index);
+}
+
 //Checks if a node is a descendant of another node
 //Function based on code by stackoverflow user 'Asaph'
 function isDescendant(parent, child){
@@ -51,14 +56,14 @@ function getTrueIndex(){
     var foundIndex = false;
     var offset = 0;
     for(var a=0; a<clientText.length; a++){
-      if(clientText[a]===">"){  //begin measuring from end of tag
+      if(clientText[a]===">"){  //begin checking from end of tag
         foundIndex = true;
         for(var b=0; b<selection.toString().length; b++){
-          if(clientText[a+1+selection.anchorOffset+b]==='<'){ //hit tag in clientText
-            offset = clientText.substring(a+1+selection.anchorOffset+b).indexOf('>')+1;
-            break;
+          if(clientText[a+1+selection.anchorOffset+b+offset]==='<'){ //hit tag in clientText
+            offset += clientText.substring(a+1+selection.anchorOffset+b+offset).indexOf('>')+1;
           }
-          else if(clientText[a+1+selection.anchorOffset+b]!==selection.toString()[b]){
+          else if(clientText[a+1+selection.anchorOffset+b+offset]!==selection.toString()[b]){
+            console.log("Check failed at",a+1+selection.anchorOffset+b,":",clientText[a+1+selection.anchorOffset+b],',',selection.toString()[b]);
             foundIndex = false;
             break;
           }
@@ -68,14 +73,14 @@ function getTrueIndex(){
           return [a+1+selection.anchorOffset, a+1+selection.anchorOffset+offset];
         }
       }
-      else if(clientText[a]===selection.toString()[0]){ //begin measuring from beginning of selection
+      else if(clientText[a]===selection.toString()[0]){ //begin checking from beginning of selection
         foundIndex = true;
         for(var c=0; c<selection.toString().length; c++){
-          if(clientText[a+c]==="<"){  //hit tag in clientText
-            offset = clientText.substring(a+c).indexOf('>')+1;
-            break;
+          if(clientText[a+c+offset]==="<"){  //hit tag in clientText
+            offset += clientText.substring(a+c).indexOf('>')+1;
           }
-          else if(clientText[a+c]!==selection.toString()[c]){
+          else if(clientText[a+c+offset]!==selection.toString()[c-offset]){
+            console.log("Check failed at",a+c,":",clientText[a+c],",",selection.toString()[c]);
             foundIndex = false;
             break;
           }
@@ -86,6 +91,8 @@ function getTrueIndex(){
         }
       }
     }
+    console.log("Did not return");
+    return null;
   }
   else{
     return null;
@@ -104,62 +111,61 @@ function toggleTagOnSelection(tag){
   var indexStart = index[0];
   var indexEnd = index[1];
 
-  var hasTag = isInTag(selection.anchorNode, tag) || isInTag(selection.focusNode, tag);
   console.log(hasTag);
-
   console.log("Selection from",indexStart,"to",indexEnd);
-  var newClientText = clientText.substring(0,indexStart)+'|'+clientText.substring(indexStart,indexEnd);
-  newClientText += '|'+clientText.substring(indexEnd);
 
-  if(false){
-    //
+  var newClientText = clientText;
+  var newSubstring;
+  var hasTag = isInTag(selection.anchorNode, tag) || isInTag(selection.focusNode, tag);
+  if(!hasTag){
+    newClientText = strInsert(newClientText, indexEnd, "</"+tag+">");
+    newClientText = strInsert(newClientText, indexStart, "<"+tag+">");
+  }
+  else{
+    newSubstring = newClientText.substring(indexStart, indexEnd);
+    if(!newSubstring.includes("<"+tag+">") && !newSubstring.includes("</"+tag+">")){
+      if(newClientText.substring(indexStart-(2+tag.length),indexStart)==="<"+tag+">"){
+        //
+      }
+      else{
+        //
+      }
+
+      if(newClientText.substring(indexEnd,indexEnd+3+tag.length)==="</"+tag+">"){
+        //
+      }
+      else{
+        //
+      }
+    }
+    else{ //tag partially holds selection
+  //   if(newClientText.substring(indexStart-(2+tag.length),indexStart)==="<"+tag+">"){  //tag starts before selection
+  //     newClientText = newClientText.substring(0, indexStart-(2+tag.length))+newClientText.substring(indexStart);
+  //     indexStart -= 2+tag.length;
+  //     indexEnd -= 2+tag.length;
+  //   }
+  //   else{ //tag starts in or far before selection
+  //     if(newSubstring.includes("<"+tag+">")){ //tag starts in selection
+  //       newSubstring = newSubstring.replace("<"+tag+">", "");
+  //       newSubstring = "<"+tag+">"+newSubstring;
+  //     }
+  //   }
+  //
+  //   if(newClientText.substring(indexEnd,indexEnd+3+tag.length)==="</"+tag+">"){
+  //     newClientText = newClientText.substring(0,indexEnd)+newClientText.substring(indexEnd+3+tag.length);
+  //   }
+  //   else{ //tag ends in or far after selection
+  //     if(newClientText.includes("</"+tag+">")){ //tag ends in selection
+  //       newSubstring = newSubstring.replace("</"+tag+">", "");
+  //       newSubstring = newSubstring+"</"+tag+">";
+  //     }
+  //     else{ //tag ends far after selection
+  //       newSubstring = newSubstring+"<"+tag+">";
+  //     }
+  //   }
+    }
   }
   updateClientText(newClientText);
-  // if(selectedText.anchorNode &&
-  //     isDescendant(document.getElementById('text-content'), selectedText.anchorNode)){
-  //   var hasTag = isInTag(selectedText.anchorNode, tag);
-  //   var newClientText;
-  //   if(!hasTag){
-  //     newClientText = clientText.substring(0, selectionStart);
-  //     newClientText += "<"+tag+">"+selectedText.toString()+"</"+tag+">";
-  //     newClientText += clientText.substring(selectionEnd);
-  //   }
-  //   else{   //text is in given tag
-  //     if(selectedText.toString().includes('<'+tag+'>') || selectedText.toString().includes('</'+tag+'>')){
-  //       if(selectedText.toString().includes('<'+tag+'>')){  //text has start of tag in it
-  //         newClientText = clientText.substring(0, selectionStart);
-  //         newClientText += '<'+tag+'>';
-  //         newClientText += selectedText.toString().replace('<'+tag+'>', '');
-  //         newClientText += clientText.substring(selectionEnd+1);
-  //       }
-  //       if(selectedText.toString().includes('</'+tag+'>')){ //text has end of tag in it
-  //         newClientText = clientText.substring(0, selectionStart);
-  //         newClientText += selectedText.toString().replace('</'+tag+'>', '');
-  //         newClientText += '</'+tag+'>';
-  //         newClientText += clientText.substring(selectionEnd+1);
-  //       }
-  //     }
-  //     else{ //selection does not contain tag
-  //       if(clientText.substring(selectionStart-(tag.length+2), selectionStart)==='<'+tag+'>'){
-  //         newClientText = clientText.substring(0, selectionStart-(tag.length+2));
-  //         newClientText += selectedText.toString();
-  //       }
-  //       else{   //tag does not start immediatly before the selected text
-  //         newClientText = clientText.substring(0, selectionStart);
-  //         newClientText += "</"+tag+">"+selectedText;
-  //       }
-  //
-  //       if(clientText.substring(selectionEnd, selectionEnd+(tag.length+4))==='</'+tag+'>'){
-  //         newClientText += clientText.substring(selectionEnd+(tag.length+3));
-  //       }
-  //       else{   //tag does not end immediatly after the selected text
-  //         newClientText += clientText.substring(selectionEnd)+'<'+tag+'>';
-  //       }
-  //     }
-  //   }
-  //   console.log(newClientText);
-  //   // updateClientText(newClientText);
-  // }
 }
 
 //Updates the file content client-side
